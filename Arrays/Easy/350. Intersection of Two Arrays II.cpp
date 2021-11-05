657281d65ae0ce21f43cf8e2602f82e4a6659252
Ayman <yaoiotakusama@gmail.com>
2021-11-05 11:05:53 +0530

    Added solution for 350

--- Arrays/Easy/350. Intersection of Two Arrays II.cpp
+++ Arrays/Easy/350. Intersection of Two Arrays II.cpp
@@ -1,16 +1,15 @@
 // HashMap Solution
-// Time Complexity: O(n + m)
+// Time Complexity: O(n + m);
 // Space Complexity: O(n)
 
 class Solution {
 public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int,int> mp;
         vector<int> result;
 
-        for(int num : nums1)
-            mp[num] = 1;
-        //if the number already exists then push it in the result and erase it
+        for(int num : nums1) mp[num]++;
+
         for(int num : nums2) {
             if(mp[num]-- > 0)
                 result.push_back(num);
