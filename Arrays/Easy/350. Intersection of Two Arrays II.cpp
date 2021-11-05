03fc929a78c17225c212b1a942bd4d38ca74b20b
Ayman <yaoiotakusama@gmail.com>
2021-11-05 11:13:24 +0530

    Added Two pointer solution for 350

--- Arrays/Easy/350. Intersection of Two Arrays II.cpp
+++ Arrays/Easy/350. Intersection of Two Arrays II.cpp
@@ -18,3 +18,31 @@ public:
         return result;
     }
 };
+
+
+// Two Pointer Solution
+// Time Complexity: O(nlogn + mlogm)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        vector<int> result;
+        
+        for(int i = 0, j = 0; i < nums1.size() && j < nums2.size();) {
+            if(nums1[i] < nums2[j])
+                i++;
+            else if(nums1[i] == nums2[j]) {
+                result.push_back(nums1[i++]);
+                j++;
+            }else {
+                j++;
+            }
+        }
+
+        return result;
+    }
+};
