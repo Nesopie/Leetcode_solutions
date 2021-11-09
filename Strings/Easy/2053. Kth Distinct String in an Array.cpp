d4700ca7f0de7b68351c6e37edd220d624887d99
Ayman <yaoiotakusama@gmail.com>
2021-11-09 20:17:29 +0530

    Added solution to 053

---
+++ Strings/Easy/2053. Kth Distinct String in an Array.cpp
@@ -0,0 +1,22 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        unordered_map<string, int> mymap;
+
+        for(string str : arr) mymap[str]++;
+
+        int counter = 0;
+
+        for(string str : arr) {
+            if(mymap[str] == 1) {
+                counter++;
+                if(counter == k) return str;
+            }
+        }
+
+        return "";
+    }
+};
