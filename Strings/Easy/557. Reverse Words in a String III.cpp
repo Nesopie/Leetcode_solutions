33a5c3cb10fd07b18d6174de00ac16abb0f597d2
Ayman <yaoiotakusama@gmail.com>
2021-11-09 06:31:46 +0530

    Added solution for 557

---
+++ Strings/Easy/557. Reverse Words in a String III.cpp
@@ -0,0 +1,49 @@
+// Stack Solution
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        stack<char> reverse;
+        string ans = "";
+
+        for(char ch : s) {
+            if(ch != ' ') {
+                reverse.push(ch);
+            }else {
+                while(!reverse.empty()) {
+                    ans += reverse.top();
+                    reverse.pop();
+                }ans += " ";
+            }
+        }
+
+        while(!reverse.empty()) {
+            ans += reverse.top();
+            reverse.pop();
+        }
+
+        return ans;
+    }
+};
+
+// Two pointer Solution
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] != ' ') {
+                int j = i;
+                for(; j < s.length() && s[j] != ' '; j++) { };
+                reverse(s.begin() + i, s.begin() + j);
+                i = j;
+            }
+        }
+
+        return s;
+    }
+};
