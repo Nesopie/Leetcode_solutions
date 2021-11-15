4864b59d0840ae8e3de59064e046c092d8a6b155
Ayman <yaoiotakusama@gmail.com>
2021-11-15 19:33:27 +0530

    Modified solution to 2027

--- Strings/Easy/2027. Minimum Moves to Convert String.cpp
+++ Strings/Easy/2027. Minimum Moves to Convert String.cpp
@@ -7,9 +7,7 @@ public:
         int minMoves = 0;
         for(int i = 0; i < s.length(); i+=3) {
             while(s[i] != 'X' && i < s.length()) i++; // skip if all zeroes
-            if((i < s.length() && s[i] == 'X') ||  //if even one is X include it and jump 3 steps
-               (i + 1 < s.length() && s[i + 1] == 'X')||
-               (i + 2 < s.length() && s[i + 2] == 'X'))
+            if((i < s.length() && s[i] == 'X')) // found an X increment by 3
                 minMoves++;
         }
 
