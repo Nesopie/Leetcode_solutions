fcbe98c605bf53421461b6de3d2a1f7bb1ce2952
Ayman <yaoiotakusama@gmail.com>
2021-11-22 21:59:18 +0530

    Added solution to 771

---
+++ Strings/Easy/771. Jewels and Stones.cpp
@@ -0,0 +1,24 @@
+// HashMap solution
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_map<char,int> isJewel;
+        for(auto x : jewels) {
+            isJewel[x] = 1;
+        }
+
+
+        int numberOfJewels = 0;
+
+        for(auto x : stones) {
+            if(isJewel.find(x) != isJewel.end()) {
+                numberOfJewels++;
+            }
+        }
+
+        return numberOfJewels;
+    }
+};
